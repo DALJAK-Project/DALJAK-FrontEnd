0ceaa4cb875620d7f9030c905454a11cc602f91e
sumi.byun <selina2000@naver.com>
2021-11-03 19:42:41 +0900

    [Feat]DaljaxCenter Component추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+// import logo from "./logo.svg";
+// import "./App.css";
+import Daljak from './Daljak/Daljak';
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+      <Daljak/>
+  </div>;
 }
 
 export default App;
