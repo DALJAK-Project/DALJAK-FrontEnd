9f47d72e9855911949c5cd2c21a267cd09b57050
sumi.byun <selina2000@naver.com>
2021-11-08 02:44:44 +0900

    [Style]html->react style

--- src/Daljak/Daljak.js
+++ src/Daljak/Daljak.js
@@ -26,36 +26,38 @@ export default function Daljak() {
     return (
         <div>
             <Nav/>
-            <div class="daljak__box">
-            <Daljak_Center title="IT" images={imgITList} boxName='it'/>
-            <Daljak_Center title="자연과학" images={imgNSList} boxName='science'/>
-            <Daljak_Center title="예체능" images={imgACTList} boxName='act' moreDisplay={true}/>
-            <Daljak_Catagory images={moreImg} />
-            <DaljakToday images={todayImg}/>
+            <div className="daljak__box">
+            <Daljak_Center  key="it" title="IT" images={imgITList} boxName='it'/>
+            <Daljak_Center  key="science" title="자연과학" images={imgNSList} boxName='science' />
+            <Daljak_Center  key="act" title="예체능" images={imgACTList} boxName='act' moreDisplay={true}/>
+            <Daljak_Catagory  key="categoty" images={moreImg} />
+            <DaljakToday  key="today" images={todayImg}/>
             </div>
         </div>
     )
 }
 function Daljak_Center({title,images,boxName,moreDisplay}) {
-    const cards = images.map((imgUrl) => (
-        <div class="daljak__box--cardlist--card">
+    const cards = images.map((imgUrl,index) => (
+        <div className="daljak__box--cardlist--card" key={`centerCardImg${index}`}>
         <img src={require(`../img/${imgUrl}`).default} alt="" />
     </div>
     ));
+    
     console.log(cards);
     return(
-        <div class="daljak__center">
-        <div class={`daljak__box--${boxName}`}>
+        <div className="daljak__center">
+        <div className={`daljak__box--${boxName}`}>
             <div className={moreDisplay?"daljak__box--it--text":''}>
-                <span class="daljak__box--it--title">{title}</span>
-                <span class="daljak__box--it--desc">전체보기</span>
+                <span className="daljak__box--it--title">{title}</span>
+                <span className="daljak__box--it--desc">전체보기</span>
             </div>
 
-            <div class="daljak__box--cardlist">
+            <div className="daljak__box--cardlist">
                 {/* 카드 */}
                 {cards}
-                <span style={{color: '#816afe'}}><i class="fas fa-play-circle fa-3x"></i>
-                </span>
+                <span style={{color: '#816afe'}}><i className="fas fa-play-circle fa-3x" aria-hidden="true"></i></span>
+                {/* <span style={{color: '#816afe'}}><i className="fas fa-play-circle fa-3x"></i>
+                </span> */}
             </div>
         </div>
     </div>
@@ -63,17 +65,17 @@ function Daljak_Center({title,images,boxName,moreDisplay}) {
 }
 
 function Daljak_Catagory({images}) {
-    const cardlist = images.map((imgUrl) => (
-        <div><img src={require(`../img/${imgUrl}`).default} alt=" " /></div>
+    const cardlist = images.map((imgUrl,index) => (
+        <div key={`categoryCardImg${index}`}><img src={require(`../img/${imgUrl}`).default} alt=" " /></div>
     ));
     return (
-        <div class="daljak__category">
-            <div class="daljak__category--box ">
-                <div class="daljak__box--category ">
-                    <div class="daljak__box--it--text--category">
-                    <span class="daljak__box--it--title ">More categories</span>
+        <div className="daljak__category">
+            <div className="daljak__category--box ">
+                <div className="daljak__box--category ">
+                    <div className="daljak__box--it--text--category">
+                    <span className="daljak__box--it--title ">More categories</span>
                     </div>
-                    <div class="daljak__box--cardlist--grid" >
+                    <div className="daljak__box--cardlist--grid" >
                         {cardlist}
                     </div>
                 </div>
@@ -83,19 +85,19 @@ function Daljak_Catagory({images}) {
 }
 
 function DaljakToday({images}) {
-    const component  = images.map((imgUrl) => (
-        <DaljakTodayComponent imgUrl={imgUrl} title="Et omnis aut id"  date="2021/3/30" desc="Et omnis aut id. Aut eaque est quaerat. Doloribus omnis fugit et sit ducimus expedita."  />
+    const component  = images.map((imgUrl,index) => (
+        <DaljakTodayComponent  key={`today${index}`} imgUrl={imgUrl} title="Et omnis aut id"  date="2021/3/30" desc="Et omnis aut id. Aut eaque est quaerat. Doloribus omnis fugit et sit ducimus expedita."  />
     ));
     return(
-        <div class=" daljak__category ">
-            <div class=" daljak__category--box--today ">
-                <div class=" daljak__box--it ">
+        <div className=" daljak__category ">
+            <div className=" daljak__category--box--today ">
+                <div className=" daljak__box--it ">
                     <div>
-                        <span class=" daljak__box--it--title ">오늘의 <span style={{"background-color":"rgba(129, 106, 254, 1)"}}>인기작</span></span>
+                        <span className=" daljak__box--it--title ">오늘의 <span style={{backgroundColor:"rgba(129, 106, 254, 1)"}}>인기작</span></span>
                     </div>
                        {component}
 
-                    <div class=" daljak__deco--bottom ">
+                    <div className=" daljak__deco--bottom ">
                         <img src=" ../img/deco-b.png " alt=" " />
                     </div>
                 </div>
@@ -107,19 +109,19 @@ function DaljakToday({images}) {
 }
 function DaljakTodayComponent({imgUrl, title, date, desc}) {
     return (
-        <div class=" daljak__box--cardlist--card--today ">
+        <div className=" daljak__box--cardlist--card--today ">
             <div>
                 <img src={require(`../img/${imgUrl}`).default} alt=" " />
             </div>
 
-            <div class=" daljak__box--cardlist--card--today--textbox ">
-                <div class=" daljak__box--cardlist--card--today--text ">
-                    <div class=" daljak__box--cardlist--card--today--title ">{title}
+            <div className=" daljak__box--cardlist--card--today--textbox ">
+                <div className=" daljak__box--cardlist--card--today--text ">
+                    <div className=" daljak__box--cardlist--card--today--title ">{title}
                     </div>
-                    <div class=" daljak__box--cardlist--card--today--date ">{date}</div>
+                    <div className=" daljak__box--cardlist--card--today--date ">{date}</div>
                 </div>
      
-                <span class=" daljak__box--cardlist--card--today--desc ">{desc}
+                <span className=" daljak__box--cardlist--card--today--desc ">{desc}
                 </span>
             </div>
         </div>
@@ -129,8 +131,8 @@ function Nav() {
     return(
         <div>
             
-            <nav class="header-bar">
-        <div class="header-bar__menu">
+            <nav className="header-bar">
+        <div className="header-bar__menu">
             <ul>
                 <li>Home</li>
                 <li>다른달작</li>
@@ -138,14 +140,14 @@ function Nav() {
                 <li>커뮤니티</li>
             </ul>
         </div>
-        <div class="header-bar__login">
-            <div class="header-bar__signup">
-                <div class="header-bar__signup--square"></div>
-                <span class="header-bar__signup--text">Sign up</span>
+        <div className="header-bar__login">
+            <div className="header-bar__signup">
+                <div className="header-bar__signup--square"></div>
+                <span className="header-bar__signup--text">Sign up</span>
             </div>
-            <span class="header-bar__login--text">Log in</span>
-            <div class="header-bar__login--profile">
-                <i class="far fa-user-circle fa-2x"></i>
+            <span className="header-bar__login--text">Log in</span>
+            <div className="header-bar__login--profile">
+                <i className="far fa-user-circle fa-2x"></i>
             </div>
         </div>
     </nav>
