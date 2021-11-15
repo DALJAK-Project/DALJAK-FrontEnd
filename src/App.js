1fdec414db21e0881269e5161c72ba72d92a3117
sumi.byun <selina2000@naver.com>
2021-11-16 04:54:02 +0900

    [Feat]Daljak_Center : Slider 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from "./logo.svg";
 // import "./App.css";
 import Daljak from './Daljak/Daljak';
-// import Test from './Test';
+import Test from './Test';
 function App() {
   return <div className="App">
       <Daljak/>
--- src/Daljak/Daljak.js
+++ src/Daljak/Daljak.js
@@ -1,196 +1,234 @@
-import React from 'react'
-import '../css/Home/header-bar.css';
+import React from "react";
+import "../css/Home/header-bar.css";
 // import '../css/Home/home-box.css';
 // import '../css/Home/header-accessories.css';
 // import '../css/Home/header-cardbox.css';
-import '../css/Daljak/daljak_home.css';
+import "../css/Daljak/daljak_home.css";
 // import '../css/Home/font.css';
-import '../css/Daljak/all.css';
+import "../css/Daljak/all.css";
 
-// slider swiper
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.min.css'
-import 'swiper/swiper.min.css'
-
+// slider React Slick
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronRight,faChevronCircleLeft,faChevronCircleRight } from "@fortawesome/free-solid-svg-icons";
-export default function Daljak() {     
-    const imgITList = [
-        "Rectangle-a.png","Rectangle-b.png","Rectangle-c.png","Rectangle-d.png","Rectangle-e.png"
-    ];
-    const imgNSList = [
-        "card-a.png","card-b.png","card-c.png","card-d.png","Rectangle-c.png"
-    ];
-    const imgACTList = [
-        "act-a.png","act-a.png","act-a.png","act-b.png","act-c.png","act-d.png","act-e.png"
-    ];
-    const moreImg = [
-        "more-a.png", "more-b.png", "more-c.png", "more-d.png", "more-e.png", "more-f.png",
-    ]
-    const todayImg = [
-        "small-a.png","small-b.png","small-c.png","small-d.png","small-e.png"
-    ]
-    return (
-        <div>
-            <Nav/>
-            
-            <div className="daljak__box">
-            <Daljak_Center1   key="it" title="IT" images={imgITList} boxName='it'/>
-            <Daljak_Center1  key="science" title="자연과학" images={imgNSList} boxName='science' />
-            <Daljak_Center2  key="act" title="예체능" images={imgACTList} boxName='act'/>
-            <Daljak_Catagory  key="categoty" images={moreImg} />
-            <DaljakToday  key="today" images={todayImg}/>
-            </div>
-        </div>
-    )
-}
-function Daljak_Center1({title,images,boxName}) {
-    const cards = images.map((imgUrl,index) => (
-        <div className="daljak__box--cardlist--card" key={`centerCardImg${index}`}>
-        <img src={require(`../img/${imgUrl}`).default} alt="" />
+import {
+  faChevronRight,
+  faChevronCircleLeft,
+  faChevronCircleRight,
+} from "@fortawesome/free-solid-svg-icons";
+export default function Daljak() {
+  const imgITList = [
+    "Rectangle-a.png",
+    "Rectangle-b.png",
+    "Rectangle-c.png",
+    "Rectangle-d.png",
+    "Rectangle-e.png",
+  ];
+  const imgNSList = [
+    "card-a.png",
+    "card-b.png",
+    "card-b.png",
+    "card-c.png",
+    "card-d.png",
+    "Rectangle-c.png",
+  ];
+  const imgACTList = [
+    "act-a.png",
+    "act-a.png",
+    "act-a.png",
+    "act-b.png",
+    "act-c.png",
+    "act-d.png",
+    "act-d.png",
+    "act-d.png",
+    "act-e.png",
+  ];
+  const moreImg = [
+    "more-a.png",
+    "more-b.png",
+    "more-c.png",
+    "more-d.png",
+    "more-e.png",
+    "more-f.png",
+  ];
+  const todayImg = [
+    "small-a.png",
+    "small-b.png",
+    "small-c.png",
+    "small-d.png",
+    "small-e.png",
+  ];
+  return (
+    <div>
+      <Nav />
+
+      <div className="daljak__box">
+        <Daljak_Center 
+        key="it" title="IT" 
+        images={imgITList} 
+        boxName="it" 
+        showSlide={5}/>
+        <Daljak_Center
+          key="science"
+          title="자연과학"
+          images={imgNSList}
+          boxName="science"
+          showSlide={5}
+        />
+        <Daljak_Center
+          key="act"
+          title="예체능"
+          images={imgACTList}
+          boxName="act"
+          showSlide={7}
+        />
+        <Daljak_Catagory key="categoty" images={moreImg} />
+        <DaljakToday key="today" images={todayImg} />
+      </div>
     </div>
-    ));
-    
-    // console.log(cards);
-    return(
-        <div className="daljak__center">
-        <div className={`daljak__box--${boxName}`}>
-            <div>
-                <span className="daljak__box--it--title">{title}</span>
-                <span className="daljak__box--it--desc">전체보기</span>
-            </div>
-
-            <div className="daljak__box--cardlist">
-                <FontAwesomeIcon icon={faChevronCircleLeft} style={{color: '#816afe'}} />
-                {/* 카드 */}
-                {cards}
-                <FontAwesomeIcon icon={faChevronCircleRight} style={{color: '#816afe'}} />
-
-                {/* <span style={{color: '#816afe'}}><MdPlayCircle size="40"/></span> */}
-                {/* <span style={{color: '#816afe'}}><i className="fas fa-play-circle fa-3x"></i>
-                </span> */}
-            </div>
-        </div>
-    </div>
-    )
+  );
 }
-function Daljak_Center2({title,images,boxName}) {
-    const cards = images.map((imgUrl,index) => (
-        <div className="daljak__box--cardlist--card" key={`centerCardImg${index}`}>
-        <img src={require(`../img/${imgUrl}`).default} alt="" />
+function Daljak_Center({ title, images, showSlide }) {
+  const cards = images.map((imgUrl, index) => (
+    <div className="daljak__box--cardlist--card" key={`centerCardImg${index}`}>
+      <img src={require(`../img/${imgUrl}`).default} alt="" />
     </div>
-    ));
-    return(
-        <div className="daljak__center">
-        <div className={`daljak__box--${boxName}`}>
-            <div>
-                <span className="daljak__box--it--title">{title}</span>
-                <span className="daljak__box--it--desc">전체보기</span>
-            </div>
-
-            <div className="daljak__box--cardlist">
-                <FontAwesomeIcon icon={faChevronCircleLeft} style={{color: '#816afe'}} />
-                {/* 카드 */}
-                {cards}
-                <FontAwesomeIcon icon={faChevronCircleRight} style={{color: '#816afe'}} />
-
-                {/* <span style={{color: '#816afe'}}><MdPlayCircle size="40"/></span> */}
-                {/* <span style={{color: '#816afe'}}><i className="fas fa-play-circle fa-3x"></i>
-                </span> */}
-            </div>
+  ));
+  const settings = {
+    // dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: showSlide,
+    slidesToScroll: 1,
+  };
+  const BoxStyle = {
+      width: `${250 * showSlide + 30}px`
+  }
+  return (
+    <div className="daljak__center">
+      <div className="daljak__box__container">
+        <div className="daljak__box--text">
+          <span className="daljak__box--title">{title}</span>
+          <span className="daljak__box--desc">전체보기</span>
         </div>
+       
+        <div style={BoxStyle}>
+        {/* <FontAwesomeIcon
+            icon={faChevronCircleLeft}
+            style={{ color: "#816afe" }}
+          /> */}
+          <Slider {...settings}>{cards}</Slider>
+          {/* <FontAwesomeIcon
+            icon={faChevronCircleRight}
+            style={{ color: "#816afe" }}
+          /> */}
+        </div>
+        
+      </div>
     </div>
-
-    )
+  );
 }
-function Daljak_Catagory({images}) {
-    const cardlist = images.map((imgUrl,index) => (
-        <div key={`categoryCardImg${index}`}><img src={require(`../img/${imgUrl}`).default} alt=" " /></div>
-    ));
-    return (
-        <div className="daljak__category">
-            <div className="daljak__category--box ">
-                <div className="daljak__box--category ">
-                    <div className="daljak__box--it--text--category">
-                    <span className="daljak__box--it--title ">More categories</span>
-                    </div>
-                    <div className="daljak__box--cardlist--grid" >
-                        {cardlist}
-                    </div>
-                </div>
-            </div>
+function Daljak_Catagory({ images }) {
+  const cardlist = images.map((imgUrl, index) => (
+    <div key={`categoryCardImg${index}`}>
+      <img src={require(`../img/${imgUrl}`).default} alt=" " />
+    </div>
+  ));
+  return (
+    <div className="daljak__category">
+      <div className="daljak__category--box ">
+        <div className="daljak__box--category ">
+          <div className="daljak__box--it--text--category">
+            <span className="daljak__box--it--title ">More categories</span>
+          </div>
+          <div className="daljak__box--cardlist--grid">{cardlist}</div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-function DaljakToday({images}) {
-    const component  = images.map((imgUrl,index) => (
-        <DaljakTodayComponent  key={`today${index}`} imgUrl={imgUrl} title="Et omnis aut id"  date="2021/3/30" desc="Et omnis aut id. Aut eaque est quaerat. Doloribus omnis fugit et sit ducimus expedita."  />
-    ));
-    return(
-        <div className=" daljak__category ">
-            <div className=" daljak__category--box--today ">
-                <div className=" daljak__box--it ">
-                    <div>
-                        <span className=" daljak__box--it--title ">오늘의 <span style={{backgroundColor:"rgba(129, 106, 254, 1)"}}>인기작</span></span>
-                    </div>
-                       {component}
+function DaljakToday({ images }) {
+  const component = images.map((imgUrl, index) => (
+    <DaljakTodayComponent
+      key={`today${index}`}
+      imgUrl={imgUrl}
+      title="Et omnis aut id"
+      date="2021/3/30"
+      desc="Et omnis aut id. Aut eaque est quaerat. Doloribus omnis fugit et sit ducimus expedita."
+    />
+  ));
+  return (
+    <div className=" daljak__category ">
+      <div className=" daljak__category--box--today ">
+        <div className=" daljak__box--it ">
+          <div>
+            <span className=" daljak__box--it--title ">
+              오늘의{" "}
+              <span style={{ backgroundColor: "rgba(129, 106, 254, 1)" }}>
+                인기작
+              </span>
+            </span>
+          </div>
+          {component}
 
-                    {/* <div className=" daljak__deco--bottom ">
+          {/* <div className=" daljak__deco--bottom ">
                         <img src=" ../img/deco-b.png " alt=" " />
                     </div> */}
-                </div>
-                
-            </div>
         </div>
-
-    )
+      </div>
+    </div>
+  );
 }
-function DaljakTodayComponent({imgUrl, title, date, desc}) {
-    return (
-        <div className=" daljak__box--cardlist--card--today ">
-            <div>
-                <img src={require(`../img/${imgUrl}`).default} alt=" " />
-            </div>
+function DaljakTodayComponent({ imgUrl, title, date, desc }) {
+  return (
+    <div className=" daljak__box--cardlist--card--today ">
+      <div>
+        <img src={require(`../img/${imgUrl}`).default} alt=" " />
+      </div>
 
-            <div className=" daljak__box--cardlist--card--today--textbox ">
-                <div className=" daljak__box--cardlist--card--today--text ">
-                    <div className=" daljak__box--cardlist--card--today--title ">{title}
-                    </div>
-                    <div className=" daljak__box--cardlist--card--today--date ">{date}</div>
-                </div>
-     
-                <span className=" daljak__box--cardlist--card--today--desc ">{desc}
-                </span>
-            </div>
+      <div className=" daljak__box--cardlist--card--today--textbox ">
+        <div className=" daljak__box--cardlist--card--today--text ">
+          <div className=" daljak__box--cardlist--card--today--title ">
+            {title}
+          </div>
+          <div className=" daljak__box--cardlist--card--today--date ">
+            {date}
+          </div>
         </div>
-    )
+
+        <span className=" daljak__box--cardlist--card--today--desc ">
+          {desc}
+        </span>
+      </div>
+    </div>
+  );
 }
 function Nav() {
-    return(
-        <div>
-            
-            <nav className="header-bar">
+  return (
+    <div>
+      <nav className="header-bar">
         <div className="header-bar__menu">
-            <ul>
-                <li>Home</li>
-                <li>다른달작</li>
-                <li>내달작</li>
-                <li>커뮤니티</li>
-            </ul>
+          <ul>
+            <li>Home</li>
+            <li>다른달작</li>
+            <li>내달작</li>
+            <li>커뮤니티</li>
+          </ul>
         </div>
         <div className="header-bar__login">
-            <div className="header-bar__signup">
-                <div className="header-bar__signup--square"></div>
-                <span className="header-bar__signup--text">Sign up</span>
-            </div>
-            <span className="header-bar__login--text">Log in</span>
-            <div className="header-bar__login--profile">
-                <i className="far fa-user-circle fa-2x"></i>
-            </div>
+          <div className="header-bar__signup">
+            <div className="header-bar__signup--square"></div>
+            <span className="header-bar__signup--text">Sign up</span>
+          </div>
+          <span className="header-bar__login--text">Log in</span>
+          <div className="header-bar__login--profile">
+            <i className="far fa-user-circle fa-2x"></i>
+          </div>
         </div>
-    </nav>
-        </div>
-    )
+      </nav>
+    </div>
+  );
 }
