362137ef71aa41d995f70f021af5436b663c428f
dongyu-youn <donghe1472@naver.com>
2021-11-20 15:12:26 +0900

    [Update] 홈화면 수정중

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,18 +1,18 @@
 import React from "react";
-import {Header} from "../Home/Components/Header";
-import {Box} from "../Home/Components/Box";
+import { Header } from "../Home/Components/Header";
+import { Box } from "../Home/Components/Box";
 import Cardbox from "./Components/Cardbox";
 import { Accessories } from "./Components/Accessories";
 
-function HomePresenter(){
-    return(
-        <div>
-            <Header />
-            <Box/>
-            <Cardbox/>
-            <Accessories />
-        </div>
-    );
+function HomePresenter() {
+  return (
+    <div className="home">
+      <Nav />
+      <Box />
+      <Cardbox />
+      <Accessories />
+    </div>
+  );
 }
 
 export default HomePresenter;
