23a7a7b1801a091272d446fafa84393432f73fb2
dongyu-youn <donghe1472@naver.com>
2021-11-22 10:20:40 +0900

    [Update] homebox-css 전체 수정

--- src/Routes/Home/Components/Box.js
+++ src/Routes/Home/Components/Box.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../../../css/Home/home-box.css";
+import logo from "../../../public/img/logo.png";
 
 const Blue = ({ title, desc }) => (
   <div class="home-box__review--blue">
@@ -30,41 +31,44 @@ export function Box() {
     <div class="home-box">
       <div class="home-box__logo">
         <div class="home-box__img">
-          <img src="../img/logo.png" alt="" />
+          <img src={logo} className="logoImage" alt="iPhone_01" />
         </div>
         <div class="home_accessories"></div>
       </div>
-      <div class="home-box__review">
-        <Blue
-          title="보람찬 대학생활의 마무리"
-          desc="여기에 졸업작품을 올리고 누군가한테 도움을 줌으로서 진정한 대학 생활의 보람찬 마무리를 할수 있게 되었습니다"
-        />
-        <Orange
-          title="감사합니다ㅠㅜ"
-          desc="여기에 올린 웹사이트로 누구한테 도움을 주게 되었고 진정으로 보람찬 대학생활로 마무리 지을수 있게되었습니다"
-        />
-        <Purple
-          title="감동을 주는 웹사이트!"
-          desc="종일 밤새도 겨우 학교에 제출할수 있는거 밖에 없었는데 너무 좋았습니다"
-        />
-        <div class="home-box__review--white">
-          <div class="home-box__review--white--main">
-            <div class="home-box__review--white--titlebox">
-              <div class="home-box__review--blue__title">후기 게시 등록</div>
-              <div>
-                <i class="material-icons">add_circle_outline</i>
+      <div class="home-box__right">
+        <div class="home-box__review">
+          <Blue
+            title="보람찬 대학생활의 마무리"
+            desc="여기에 졸업작품을 올리고 누군가한테 도움을 줌으로서 진정한 대학 생활의 보람찬 마무리를 할수 있게 되었습니다"
+          />
+          <Orange
+            title="감사합니다ㅠㅜ"
+            desc="여기에 올린 웹사이트로 누구한테 도움을 주게 되었고 진정으로 보람찬 대학생활로 마무리 지을수 있게되었습니다"
+          />
+          <Purple
+            title="감동을 주는 웹사이트!"
+            desc="종일 밤새도 겨우 학교에 제출할수 있는거 밖에 없었는데 너무 좋았습니다"
+          />
+          <div class="home-box__review--white">
+            <div class="home-box__review--white--main">
+              <div class="home-box__review--white--titlebox">
+                <div class="home-box__review--blue__title">후기 게시 등록</div>
+                <div>
+                  <i class="material-icons">add_circle_outline</i>
+                </div>
               </div>
+              <span class="home-box__review--blue__desc">
+                여러분의 달작 이용 후기를 작성해주세요!
+              </span>
             </div>
-            <span class="home-box__review--blue__desc">
-              여러분의 달작 이용 후기를 작성해주세요!
-            </span>
           </div>
         </div>
-      </div>
-      <div class="home-box__dot">
-        <p class="v10"></p>
-        <p class="v10"></p>
-        <p class="v10"></p>
+
+        <div class="home-box__dot">
+          <p class="v10"></p>
+          <p class="v10"></p>
+          <p class="v10"></p>
+        </div>
       </div>
     </div>
   );
--- src/Routes/Home/Components/Cardbox.js
+++ src/Routes/Home/Components/Cardbox.js
@@ -1,58 +1,58 @@
 import React from "react";
-import '../../../css/Home/home-cardbox.css';
+import "../../../css/Home/home-cardbox.css";
 import styled from "styled-components";
 import Img1 from "../../../img/bad.png";
-import Img2 from "../../../img/frame.png"
-import Img3 from "../../../img/music.png"
-import Img4 from "../../../img/photo.png"
-import Img5 from "../../../img/arc.png"
-import Img6 from "../../../img/news.png"
-import Img7 from "../../../img/podcast.png"
+import Img2 from "../../../img/frame.png";
+import Img3 from "../../../img/music.png";
+import Img4 from "../../../img/photo.png";
+import Img5 from "../../../img/arc.png";
+import Img6 from "../../../img/news.png";
+import Img7 from "../../../img/podcast.png";
 const StyledSpan = styled.span`
-    background-color:rgba(129, 106, 254, 1);
+  background-color: rgba(129, 106, 254, 1);
 `;
-const TopImg=styled.img`
-    width: 340.72px;
-    height: 168.53px;
+const TopImg = styled.img`
+  width: 340.72px;
+  height: 168.53px;
 `;
-const OutterImg=styled.img`
-    width: 708px;
-    height: 169px;
+const OutterImg = styled.img`
+  width: 708px;
+  height: 169px;
 `;
-const InnerImg=styled.img`
-    width: 341px;
-    height: 168.53px;
+const InnerImg = styled.img`
+  width: 341px;
+  height: 168.53px;
 `;
-const Input=styled.input`
-    color: #c0c0c0;
-    font-family:Noto Sans;
-    font-weight: Regular;
-    font-size:16px;
-    opacity:1;
-    padding-left:20px;
-    width:100%;
-    height:100%;
-    border-radius:50px;
-    border:none;
-    outline:none;
+const Input = styled.input`
+  color: #c0c0c0;
+  font-family: Noto Sans;
+  font-weight: Regular;
+  font-size: 16px;
+  opacity: 1;
+  padding-left: 20px;
+  width: 100%;
+  height: 100%;
+  border-radius: 50px;
+  border: none;
+  outline: none;
 `;
-const Form=styled.form`
-    position: absolute;
-    top: 1060px;
-    width: 660px;
-    height: 67px;
-    display: flex;
-    background: #ffffff;
-    /* Primary/Purple_dark */
-    border: 2px solid #6759f6;
-    box-sizing: border-box;
-    border-radius: 50px;
-    justify-content: flex-start;
-    align-items: center;
+const Form = styled.form`
+  position: absolute;
+  top: 1060px;
+  width: 660px;
+  height: 67px;
+  display: flex;
+  background: #ffffff;
+  /* Primary/Purple_dark */
+  border: 2px solid #6759f6;
+  box-sizing: border-box;
+  border-radius: 50px;
+  justify-content: flex-start;
+  align-items: center;
 `;
 /*Search for music, art, culture...*/
-class Cardbox extends React.Component{
-    /*constructor(props){
+class Cardbox extends React.Component {
+  /*constructor(props){
         super(props);
         this.state={
             searchTerm:"",
@@ -67,41 +67,62 @@ class Cardbox extends React.Component{
             this.searchByterm()
         }
     }*/
-    render(){
-        return(
-            <div class="home-cardbox">
-                <div class="v2_1348">
-                    <div class="home-cardbox__text">
-                        <span class="v2_1350">우리들의 <StyledSpan>작품</StyledSpan>을 공유하는 공간</span>
-                    </div>
-                </div>
-                <div class="home-cardbox__text--main">
-                    세상에 하나뿐인 작품을 이곳에 공유해 보세요!
-                </div>
-                <Form>
-                    <Input
-                        placeholder="Search for music, art, culture..."
-                    />
-                </Form>
-                <div class="home-cardbox__tag">
-                    <span class="v44_554">#rock</span>
-                    <span class="v44_555">#architecture</span>
-                    <span class="v44_556">#news</span>
-                    <span class="v44_557">#art</span>
-                    <span class="v44_558">#badminton</span>
-                    <span class="v44_559">#comedy</span>
-                </div>
-                <div class="home-cardbox__card">
-                    <span class="div"><TopImg src={Img1}/></span>
-                    <span class="div"><TopImg src={Img2}/></span>
-                    <span class="div"><TopImg src={Img3}/></span>
-                    <span class="div"><OutterImg src={Img4}/></span>
-                    <span class="div"><InnerImg src={Img5}/></span>
-                    <span class="div"><InnerImg src={Img6}/></span>
-                    <span class="div"><OutterImg src={Img7}/></span>
-                </div>
-            </div>
-        );
-    }
-};
+  render() {
+    return (
+      <div class="home-cardbox">
+        {/* <div class="v2_1348">
+        <div class="home-cardbox__text">
+          <span class="v2_1350">
+            우리들의 <StyledSpan>작품</StyledSpan>을 공유하는 공간
+          </span>
+        </div>
+        </div> */}
+
+        <div class="home-cardbox__text">
+          <span>
+            우리들의
+            <StyledSpan>작품</StyledSpan>을 공유하는 공간
+          </span>
+        </div>
+        <div class="home-cardbox__text--main">
+          세상에 하나뿐인 작품을 이곳에 공유해 보세요!
+        </div>
+        <Form>
+          <Input placeholder="Search for music, art, culture..." />
+        </Form>
+        <div class="home-cardbox__tag">
+          <span class="v44_554">#rock</span>
+          <span class="v44_555">#architecture</span>
+          <span class="v44_556">#news</span>
+          <span class="v44_557">#art</span>
+          <span class="v44_558">#badminton</span>
+          <span class="v44_559">#comedy</span>
+        </div>
+        <div class="home-cardbox__card">
+          <span class="div">
+            <TopImg src={Img1} />
+          </span>
+          <span class="div">
+            <TopImg src={Img2} />
+          </span>
+          <span class="div">
+            <TopImg src={Img3} />
+          </span>
+          <span class="div">
+            <OutterImg src={Img4} />
+          </span>
+          <span class="div">
+            <InnerImg src={Img5} />
+          </span>
+          <span class="div">
+            <InnerImg src={Img6} />
+          </span>
+          <span class="div">
+            <OutterImg src={Img7} />
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
 export default Cardbox;
