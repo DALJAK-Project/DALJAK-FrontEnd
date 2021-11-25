3e25183a1610a1af897804a9ed8e5c7b731f40c9
sumi.byun <selina2000@naver.com>
2021-11-26 02:29:34 +0900

    [Feat]otherDaljakPage Title add

--- src/Daljak/DaljakDetail.js
+++ src/Daljak/DaljakDetail.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React from 'react';
 import Nav from "./Nav";
 
+import '../css/Daljak/daljak_detail.css';
 function DaljakDetail() {
     return (
         
-        <div>
-        <Nav/>
-            Hello
+        <div className="Daljak-DetailPage">
+            <Nav/>
+            <div className="Daljak-DetailPage-container">
+                 <div className="page-title">
+                    <span className="title-text">당신이 관심있는 작품을 찾아보세요</span>
+                 </div>
+                <div className="search-container">
+                    <input className="search-bar"/>
+                    <i className="search-bar-icon">icon</i>
+                    <div className="search-tag">
+                        <ul>
+                            <li className="tag-item">#rock</li>
+                            <li className="tag-item">#architecture</li>
+                            <li className="tag-item">#news</li>
+                            <li className="tag-item">#art</li>
+                            <li className="tag-item">#badminton</li>
+                            <li className="tag-item">#comedy</li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
