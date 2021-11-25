266e38ea2045e65e178a6315a84679ec32fb59d9
sumi.byun <selina2000@naver.com>
2021-11-25 23:54:45 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 // import logo from "./logo.svg";
 // import "./App.css";
 import Daljak from './Daljak/Daljak';
-import Test from './Test';
 function App() {
   return <div className="App">
       <Daljak/>
-      {/* <Test/> */}
   </div>;
 }
 
