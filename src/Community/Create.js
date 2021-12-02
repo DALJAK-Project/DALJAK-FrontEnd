b9d0ec7ac526673744733d93ae1d7b5a31d9bda1
dongyu-youn <donghe1472@naver.com>
2021-12-02 11:30:13 +0900

    [Feat] create 완성 99.9%

--- src/Community/Create.js
+++ src/Community/Create.js
@@ -1,4 +1,4 @@
-import react, { Component } from "react";
+import react, { Component, useState } from "react";
 import community_crate from "../css/community/community_create.css";
 import Nav from "../components/Nav";
 // NOTE: Use the editor from source (not a build)!
@@ -10,11 +10,6 @@ import styled from "styled-components";
 import Dropdown from "./Dropdown";
 import Subject from "./Subject";
 
-// 여기서 일단 fontawsome import 시키고
-// 자 여기서 부터 create 값을 입력받을시 e객체로 target값들 콘솔에다가 입력시켜보자
-
-// 입력받을시 e.target.value값 받아와보자
-
 const Dropdown_box = styled.div`
   display: ${(props) => (props.isToggle === "false" ? "none" : "block")};
 `;
@@ -23,171 +18,167 @@ const DropdownMenu = styled.div`
   display: ${(props) => (props.sign === "+" ? "none" : "block")};
 `;
 
-// 버튼에다가 onchange만들어줘서 이쪽 메뉴에다가 클릭이벤트 mode = 로직을 클릭한 로직으로
-// 나 뭐하고 있었지...? 아 그래서 mode를 바꾸면 그 입력한 글자 그대로 가기로 한거지?
+function Create() {
+  const [index, setindex] = useState("xx");
 
-class Create extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      category_num: 1,
-      contents: { title: "", contents: "" },
-      dropdown: "+",
-      // 여기서 다운기호 쓰려면 어떻게 해야하지??
-      mode: "category",
-      menu: [
-        { id: 1, title: "it" },
-        { id: 2, title: "인문" },
-        { id: 3, title: "자연" },
-        { id: 4, title: "예체능" },
-      ],
-      isToggleOn: "true",
-    };
-  }
+  const [viewContent, setViewContent] = useState([]);
+  const [inputContent, setInputContent] = useState({
+    title: "",
+    tag: "",
+    content: "",
+  });
 
-  // 그럼 꼭 다 만들어가야하나..?
+  const onChange = (e) => setInputContent(e.target.value);
 
-  // 로직: 한개를 클릭했을때 mode값이 그대로 바뀜
-  DropdownClickHandler(event) {
-    // 클릭했을때
-    this.setState((state) => ({
-      dropdown: state.dropdown === "+" ? "-" : "+",
-      isToggleOn: "true",
-    }));
-  }
-  // i가 3보다 작을경우 배열을 반복해서 id값과 맞춤
-  render() {
-    // var i = 0;
-    // var title = null;
-    // while(i<4) {
-    //   var data = this.state.menu[i];
-    //   if()
-    //   // data안에 배열과 아이디값이 똑같을경우
-    // }
-    var _title = null;
-    var i = 0;
-    while (i < this.state.menu.length) {
-      var data = this.state.menu[i];
-      if (data.id === this.state.category_num) {
-        _title = data.title;
-      }
-      i = i + 1;
-    }
+  const onSumbit = (e) => {
+    setViewContent(viewContent.concat({ ...inputContent }));
+  };
 
-    return (
-      <div>
-        <Nav> </Nav>{" "}
-        <div class="community-create">
-          <h1 class="community-create__text"> 커뮤니티 글쓰기 </h1>{" "}
-          <div class="community-create__box">
-            <form class="community-create__form" action="" method="post">
-              <p>
-                <input
-                  //   onSubmit={function (e) {
-                  //     e.prventDefault();
-                  //     alert("섭밑!!");
-                  //     this.props;
-                  //   }}
-                  class="community-create__input"
-                  type="text"
-                  name="title"
-                  id=""
-                  placeholder="제목"
-                />
-              </p>{" "}
-              <p>
-                <input
-                  //   onSubmit={function (e) {
-                  //     e.prventDefault();
-                  //     alert("섭밑!!");
-                  //     this.props;
-                  //   }}
-                  class="community-create__input"
-                  type="text"
-                  name="title"
-                  id=""
-                  placeholder="태그"
-                />
-              </p>{" "}
-              {/* 여기서 로직: 만약 바뀐 모드가 state모드값과 같다면 ttile값 출력 */}{" "}
-              <p>
-                <input
-                  type="file"
-                  id="chooseFile"
-                  name="chooseFile"
-                  accept="image/*"
-                  onchange="loadFile(this)"
-                />{" "}
-              </p>{" "}
-              {/* 그럼여기서 이거를 on submit했을때 target값을 받아와야해 */}{" "}
-              {/* 머리가 하얗다..  */}{" "}
-              <p class="community-create__p">
-                <div>
-                  <button
-                    //   onSubmit={function (e) {
-                    //     e.prventDefault();
-                    //     alert("섭밑!!");
-                    //     this.props;
-                    //   }}
-                    type="button"
-                    class="community-create__input"
-                    placeholder="학과명"
-                    onClick={this.DropdownClickHandler.bind(this)}
-                  >
-                    {_title} {/* 이거의 타이틀만 바꾸면되는데 */}{" "}
-                  </button>{" "}
-                  {/* 클릭하면 안보이게 => state값이 1보다 크면 display none으로  */}{" "}
-                  {this.state.dropdown}{" "}
-                </div>{" "}
-                {/* 메뉴박스들 */} {/* 무슨태그로..? */}{" "}
-                <DropdownMenu sign={this.state.dropdown}>
-                  <Dropdown_box isToggle={this.state.isToggleOn}>
-                    <Dropdown
-                      data={this.state.menu}
-                      onChangeDropdwon={function (id) {
-                        this.setState({
-                          category_num: Number(id),
-                          isToggleOn:
-                            this.state.isToggleOn === "true" ? "false" : "true",
-                          dropdown: "+",
-                        });
-                        // 클릭하면 display none으로
-                      }.bind(this)}
-                    ></Dropdown>{" "}
-                  </Dropdown_box>{" "}
-                </DropdownMenu>{" "}
-              </p>{" "}
-              {/* 클릭했을때 카테고리 이름 변경되게끔 */}{" "}
-              {/* h 여기다 드롭다운 만들것이다 */}{" "}
-              {/* onclick -> display block 이였던게 보이게끔 */}{" "}
-              {/* 로직은 아코디언이랑 비슷 ] */}{" "}
-              {/* 아코디언 로직 -> + -로 변하면서 밑에있던것들이 보인다 */}{" "}
-              <CKEditor
-                editor={ClassicEditor}
-                onReady={(editor) => {
-                  // You can store the "editor" and use when it is needed.
-                  console.log("Editor is ready to use!", editor);
-                }}
-                onChange={(event, editor) => {
-                  const data = editor.getData();
-                  console.log({ event, editor, data });
-                }}
-                onBlur={(event, editor) => {
-                  console.log("Blur.", editor);
-                }}
-                onFocus={(event, editor) => {
-                  console.log("Focus.", editor);
-                }}
+  const onSelect = (e) => {
+    setindex(e.target.value);
+  };
+
+  const getValue = (e) => {
+    const { name, value } = e.target;
+    setInputContent({
+      ...inputContent,
+      [name]: value,
+    });
+    console.log(inputContent);
+  };
+  return (
+    <div>
+      <Nav> </Nav>{" "}
+      <div class="community-create">
+        <h1 class="community-create__text"> 커뮤니티 글쓰기 </h1>{" "}
+        <div class="community-create__box">
+          <form class="community-create__form" action="" onSubmit={onSumbit}>
+            {/* 폼 class이름을 변경 */}
+            <p>
+              <input
+                //   onSubmit={function (e) {
+                //     e.prventDefault();
+                //     alert("섭밑!!");
+                //     this.props;
+                //   }}
+                className="community-create__input"
+                type="text"
+                name="title"
+                onChange={getValue}
+                // onchange돌릴때 이 value 값을 남기도록
+
+                placeholder="제목"
+              />
+            </p>{" "}
+            {/* 이거 두개를 따로 만들어야한다 */}
+            <p>
+              {/* o 왜 같이 입력값이 나올까 */}
+              <input
+                //   onSubmit={function (e) {
+                //     e.prventDefault();
+                //     alert("섭밑!!");
+                //     this.props;
+                //   }}
+                class="community-create__input"
+                type="text"
+                name="tag"
+                onChange={getValue}
+                id=""
+                placeholder="태그"
+                // onChange={onChange}
+              />
+            </p>{" "}
+            {/* 여기서 로직: 만약 바뀐 모드가 state모드값과 같다면 ttile값 출력 */}{" "}
+            <p>
+              <input
+                type="file"
+                id="chooseFile"
+                name="chooseFile"
+                accept="image/*"
+                onchange="loadFile(this)"
               />{" "}
-            </form>{" "}
-          </div>{" "}
-          <Subject title="제목" desc="부제목"></Subject>
+            </p>{" "}
+            {/* 그럼여기서 이거를 on submit했을때 target값을 받아와야해 */}{" "}
+            {/* 머리가 하얗다..  */}{" "}
+            <p class="community-create__p">
+              <div>
+                <select
+                  //   onSubmit={function (e) {
+                  //     e.prventDefault();
+                  //     alert("섭밑!!");
+                  //     this.props;
+                  //   }}
+
+                  type="button"
+                  class="community-create__input"
+                  placeholder="학과명"
+                  onChange={onSelect}
+                  value={index}
+                >
+                  <option value="xx">과를 선택해주세요</option>
+                  <option value="0">it융합</option>
+                  <option value="1">자연과학</option>
+                  <option value="2">인문</option>
+                  <option value="3">예체능 </option>
+                </select>{" "}
+              </div>{" "}
+              {/* 메뉴박스들 */} {/* 무슨태그로..? */}{" "}
+              {/* <DropdownMenu sign={this.state.dropdown}>
+                <Dropdown_box isToggle={this.state.isToggleOn}>
+                  <Dropdown
+                    data={this.state.menu}
+                    onChangeDropdwon={function (id) {
+                      this.setState({
+                        category_num: Number(id),
+                        isToggleOn:
+                          this.state.isToggleOn === "true" ? "false" : "true",
+                        dropdown: "+",
+                      });
+                      // 클릭하면 display none으로
+                    }.bind(this)}
+                  ></Dropdown>{" "}
+                </Dropdown_box>{" "}
+              </DropdownMenu>{" "} */}
+            </p>{" "}
+            {/* 클릭했을때 카테고리 이름 변경되게끔 */}{" "}
+            {/* h 여기다 드롭다운 만들것이다 */}{" "}
+            {/* onclick -> display block 이였던게 보이게끔 */}{" "}
+            {/* 로직은 아코디언이랑 비슷 ] */}{" "}
+            {/* 아코디언 로직 -> + -로 변하면서 밑에있던것들이 보인다 */}{" "}
+            <CKEditor
+              editor={ClassicEditor}
+              onReady={(editor) => {
+                // You can store the "editor" and use when it is needed.
+                console.log("Editor is ready to use!", editor);
+              }}
+              onChange={(event, editor) => {
+                const data = editor.getData();
+                console.log({ event, editor, data });
+                setInputContent({
+                  ...inputContent,
+                  content: data,
+                });
+                console.log(inputContent);
+              }}
+              onBlur={(event, editor) => {
+                console.log("Blur.", editor);
+              }}
+              onFocus={(event, editor) => {
+                console.log("Focus.", editor);
+              }}
+            />{" "}
+            <button className="community-create__submit-button"></button>
+          </form>{" "}
+          {viewContent.map((element) => (
+            <div>
+              <h2>{element.title}</h2>
+              <div>{element.content}</div>
+            </div>
+          ))}
         </div>{" "}
-      </div>
-    );
-    //   잴 먼저 해야할것 form을 만들자
-    // onsubmit했을때 받을것들
-  }
+      </div>{" "}
+    </div>
+  );
 }
 
 export default Create;
--- src/Community/Recent.js
+++ src/Community/Recent.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import "../css/community/community__topbox.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronRight,
+  faChevronLeft,
+} from "@fortawesome/free-solid-svg-icons";
 import Slider from "react-slick";
 import "../css/slick.css";
 import "../css/slick-theme.css";
@@ -12,32 +15,49 @@ const Container = styled.div`
   overflow: hidden;
 `;
 
+// state를 업데이트시킬때마다 이미지 이동
+
 class Recent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slide: 0,
+    };
+  }
   render() {
-    const settings = {
-      slide: "div", //슬라이드 되어야 할 태그 ex) div, li
-      infinite: true, //무한 반복 옵션
-      slidesToShow: 3, // 한 화면에 보여질 컨텐츠 개수
-      slidesToScroll: 1, //스크롤 한번에 움직일 컨텐츠 개수
-      speed: 50, // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
-      arrows: true, // 옆으로 이동하는 화살표 표시 여부
-      dots: true, // 스크롤바 아래 점으로 페이지네이션 여부
-      autoplay: true, // 자동 스크롤 사용 여부
-      autoplaySpeed: 1000, // 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
-      pauseOnHover: true, // 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
-      vertical: false, // 세로 방향 슬라이드 옵션
-      adaptiveHeight: true,
-      infinite: true,
-      centerPadding: 30,
-      spaceBetween: 30, // 슬라이드간 간격
+    const MAX_SLIDES = 3;
+    const handleBannerRight = () => {
+      if (this.state.slide >= MAX_SLIDES) {
+        this.setState({
+          slide: 0,
+        });
+        // 3보다 클경우 0으로 돌아간다
+      } else {
+        this.setState({
+          slide: this.state.slide + 1,
+        });
+      }
+    };
+    const handleBannerLeft = () => {
+      if (this.state.slide === 0) {
+        this.setState({
+          slide: MAX_SLIDES,
+        });
+        // slideState가 0이되면 최대 갯수(마지막이미지를 위해) MAX_SLIDES로 state를 업데이트 한다.
+      } else {
+        this.setState({
+          slide: this.state.slide - 1,
+        });
+        // 클릭할때마다 1씩 감소
+      }
     };
     return (
-      <div>
-        <div class="community__recent">
-          <div class="community__recent--text">
-            <span> Recent posts </span>{" "}
-          </div>{" "}
-          <Slider {...settings}>
+      <div class="community__recent">
+        <div class="community__recent--text">
+          <span> Recent posts </span>{" "}
+        </div>{" "}
+        <div class="community__recent-box">
+          <div class="community__recent-whitebox">
             <div class="community_recent-whitebox--text">
               <h1>Making a design system from scratch</h1>{" "}
               <span>12 Feb 2020</span> <br />
@@ -66,7 +86,6 @@ class Recent extends Component {
                 Exercitation veniam consequat sunt nostrud amet.
               </span>
             </div>
-
             <div class="community_recent-whitebox--text">
               <h1>Are you crazy?</h1> <span>12 Feb 2020</span> <br />
               <br />
@@ -94,8 +113,31 @@ class Recent extends Component {
                 Exercitation veniam consequat sunt nostrud amet.
               </span>
             </div>
-          </Slider>
-          {/* o 지금 문제 상황 박스가 하나로 돌아감 */}
+            <div class="community_recent-whitebox--text">
+              <h1>Are you crazy?</h1> <span>12 Feb 2020</span> <br />
+              <br />
+              <span>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </span>
+            </div>
+            <div class="community_recent-whitebox--text">
+              <h1>Are you crazy?</h1> <span>12 Feb 2020</span> <br />
+              <br />
+              <span>
+                Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+                amet sint. Velit officia consequat duis enim velit mollit.
+                Exercitation veniam consequat sunt nostrud amet.
+              </span>
+            </div>
+            {/* o 지금 문제 상황 박스가 하나로 돌아감 */}
+          </div>
+          <FontAwesomeIcon
+            onClick={handleBannerRight}
+            icon={faChevronRight}
+            size="3x"
+          ></FontAwesomeIcon>
         </div>
       </div>
     );
